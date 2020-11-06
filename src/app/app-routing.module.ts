b422e8c03a7735dec9659ba6e65139674a9e30bb
Nishanthan96 <67302874+Nishanthan96@users.noreply.github.com>
2020-11-06 12:54:50 +0530

    dashboard edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 import {FrontpageComponent} from '../app/frontpage/frontpage.component';
 import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './Admin/dashboard/dashboard.component'
 
 const routes: Routes = [
   {path:'',component:FrontpageComponent},
   {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent}
 ];
 
 @NgModule({
