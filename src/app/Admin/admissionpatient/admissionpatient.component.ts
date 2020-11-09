32580a79274506e83e3b12b9e53ff0ea0857b422
Nishanthan96 <67302874+Nishanthan96@users.noreply.github.com>
2020-11-09 11:34:53 +0530

    ADMIN dashboard pages

---
+++ src/app/Admin/admissionpatient/admissionpatient.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-admissionpatient',
+  templateUrl: './admissionpatient.component.html',
+  styleUrls: ['./admissionpatient.component.css']
+})
+export class Admissionpatient implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,25 @@ import { from } from 'rxjs';
 import {FrontpageComponent} from '../app/frontpage/frontpage.component';
 import {LoginComponent} from './login/login.component';
 import {DashboardComponent} from './Admin/dashboard/dashboard.component'
+import { DoctorprofileComponent } from './Admin/doctorprofile/doctorprofile.component';
+import { RegisterdoctorComponent } from './Admin/registerdoctor/registerdoctor.component';
+import { RegisterpatientComponent } from './Admin/registerpatient/registerpatient.component';
+import { ScheduleappointmentsComponent } from './Admin/scheduleappointments/scheduleappointments.component';
+import { ScheduleappointmentsviewComponent } from './Admin/scheduleappointmentsview/scheduleappointmentsview.component';
+import { ViewpatientsComponent } from './Admin/viewpatients/viewpatients.component';
+import { Admissionpatient } from './Admin/admissionpatient/admissionpatient.component';
 
 const routes: Routes = [
   {path:'',component:FrontpageComponent},
   {path:'login',component:LoginComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'dashboard',component:DashboardComponent},
+  {path:'doctorprofile', component:DoctorprofileComponent},
+  {path:'regdoctor', component:RegisterdoctorComponent},
+  {path:'regpatient',component:RegisterpatientComponent},
+  {path:'scheduleapp', component:ScheduleappointmentsComponent},
+  {path:'scheduleappview', component:ScheduleappointmentsviewComponent},
+  {path:'viewpatient', component:ViewpatientsComponent},
+  {path:'admission', component:Admissionpatient}
 ];
 
 @NgModule({
