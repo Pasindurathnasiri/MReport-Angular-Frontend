4cb06ef0f3b0a9b78fb0a2c531c3d8dafc3d1bb0
razikusman <razikusman@gmail.com>
2020-11-15 23:41:45 +0530

    resolved conflict made first comit in doctor dashboard

---
+++ src/app/Doctor/doctorsidenav/doctorsidenav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctorsidenav',
+  templateUrl: './doctorsidenav.component.html',
+  styleUrls: ['./doctorsidenav.component.css']
+})
+export class DoctorsidenavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
