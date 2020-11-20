ef79d01ae6501d998a29cba30ebd176feb7ca034
Pasindu Madhuranga <45681668+Pasindurathnasiri@users.noreply.github.com>
2020-11-20 17:55:20 +0530

    register patient function done

--- src/app/shared/patient.service.ts
+++ src/app/shared/patient.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Patient} from '../shared/patient.model';
+import { HttpClient, HttpHeaders,HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { env } from 'process';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
-  constructor() { }
+  noAuthHeader = { headers: new HttpHeaders({ 'NoAuth': 'True' }) };
+
+
+  constructor(private http: HttpClient) { }
+
+  getAllPatient(){
+    return this.http.get(environment.apiBaseUrl+'/Patients');
+  }
+
+  regPatient(patient:Patient){
+    return this.http.post(environment.apiBaseUrl+'/Patients',patient,this.noAuthHeader);
+  }
 }
